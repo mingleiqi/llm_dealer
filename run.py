305d05ab0d6a60496e85edc73c1c4a874ed41e1d
wxy2ab <wxy2ab@sina.com>
2024-08-17 09:35:06 +0800

    add prompt_generate

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from dealer.run_strategy_xt import run_strategy
+from dealer.prompt_generate import PromptTemplateGenerator
+from core.llms.simple_claude import SimpleClaudeAwsClient
+from dealer.stock_data_provider import StockDataProvider
 
 if __name__ == '__main__':
-    run_strategy()
+    llm = SimpleClaudeAwsClient()
+    data = StockDataProvider(llm)
+    generator = PromptTemplateGenerator(data,llm)
+    result = generator.generate_prompt("个股行情预测")
+    print(result)
